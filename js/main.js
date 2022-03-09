69e6a01aa766293a76cff435e39a37f6168e1534
Hieu Cris <hieuhk195@gmail.com>
2022-03-09 09:20:56 +0700

    setup Api file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log('hi');
+import postApi from '../api/postApi';
+
+async function main() {
+    const queryParam = {
+        _page: 1,
+        _limit: 10,
+    };
+    const response = await postApi.getAll(queryParam);
+    console.log(response);
+}
+main();
