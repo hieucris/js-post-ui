823d762bf942778548291205706788a358762517
Hieu Cris <hieuhk195@gmail.com>
2022-03-18 10:21:19 +0700

    render-post-list

--- js/main.js
+++ js/main.js
@@ -1,11 +1,58 @@
-import postApi from '../api/postApi';
+import postApi from './api/postApi';
+import { setTextContent } from './utils';
 
-async function main() {
-    const queryParam = {
-        _page: 1,
-        _limit: 10,
-    };
-    const response = await postApi.getAll(queryParam);
-    console.log(response);
+function createPostElement(post) {
+    if (!post) return;
+    try {
+        const postTemplate = document.getElementById('postTemplate');
+        if (!postTemplate) return;
+
+        const liElement = postTemplate.content.firstElementChild.cloneNode(true);
+        if (!liElement) return;
+
+        setTextContent(liElement, '[data-id="title"]', post.title);
+        setTextContent(liElement, '[data-id="description"]', post.description);
+        setTextContent(liElement, '[data-id="author"]', post.author);
+
+        // const titleElement = liElement.querySelector('[data-id="title"]');
+        // if (titleElement) titleElement.textContent = post.title;
+
+        // const descriptionElement = liElement.querySelector('[data-id="description"]');
+        // if (descriptionElement) descriptionElement.textContent = post.description;
+
+        // const authorElement = liElement.querySelector('[data-id="author"]');
+        // if (authorElement) authorElement.textContent = post.author;
+
+        const thumbnailElement = liElement.querySelector('[data-id="thumbnail"]');
+        if (thumbnailElement) thumbnailElement.src = post.imageUrl;
+
+        return liElement;
+    } catch (error) {
+        console.log('failed create post element', error);
+    }
 }
-main();
+
+function renderPostList(postList) {
+    if (!Array.isArray(postList) || postList.length === 0) return;
+
+    const ulElement = document.getElementById('postList');
+    if (!ulElement) return;
+
+    postList.forEach((post, idx) => {
+        const liElement = createPostElement(post);
+        ulElement.appendChild(liElement);
+    });
+}
+
+(async() => {
+    try {
+        const queryParams = {
+            _page: 1,
+            _limit: 6,
+        };
+        const { data, pagination } = await postApi.getAll(queryParams);
+        renderPostList(data);
+    } catch (error) {
+        console.log('getAll failed', error);
+    }
+})();
