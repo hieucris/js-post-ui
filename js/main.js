6cc63b2f0c3f2de951a6459e772507803353302f
Hieu Cris <hieuhk195@gmail.com>
2022-03-19 15:53:28 +0700

    add dayjs-truncateText description

--- js/main.js
+++ js/main.js
@@ -1,35 +1,36 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import postApi from './api/postApi';
-import { setTextContent } from './utils';
+import { setTextContent, truncateText } from './utils';
+
+dayjs.extend(relativeTime);
 
 function createPostElement(post) {
     if (!post) return;
-    try {
-        const postTemplate = document.getElementById('postTemplate');
-        if (!postTemplate) return;
+    const postTemplate = document.getElementById('postTemplate');
+    if (!postTemplate) return;
 
-        const liElement = postTemplate.content.firstElementChild.cloneNode(true);
-        if (!liElement) return;
+    const liElement = postTemplate.content.firstElementChild.cloneNode(true);
+    if (!liElement) return;
 
-        setTextContent(liElement, '[data-id="title"]', post.title);
-        setTextContent(liElement, '[data-id="description"]', post.description);
-        setTextContent(liElement, '[data-id="author"]', post.author);
+    setTextContent(liElement, '[data-id="title"]', post.title);
+    setTextContent(liElement, '[data-id="description"]', truncateText(post.description, 300));
+    setTextContent(liElement, '[data-id="author"]', post.author);
+    setTextContent(liElement, '[data-id="timeSpan"]', `- ${dayjs(post.updateAt).fromNow()}`);
 
-        // const titleElement = liElement.querySelector('[data-id="title"]');
-        // if (titleElement) titleElement.textContent = post.title;
+    // const titleElement = liElement.querySelector('[data-id="title"]');
+    // if (titleElement) titleElement.textContent = post.title;
 
-        // const descriptionElement = liElement.querySelector('[data-id="description"]');
-        // if (descriptionElement) descriptionElement.textContent = post.description;
+    // const descriptionElement = liElement.querySelector('[data-id="description"]');
+    // if (descriptionElement) descriptionElement.textContent = post.description;
 
-        // const authorElement = liElement.querySelector('[data-id="author"]');
-        // if (authorElement) authorElement.textContent = post.author;
+    // const authorElement = liElement.querySelector('[data-id="author"]');
+    // if (authorElement) authorElement.textContent = post.author;
 
-        const thumbnailElement = liElement.querySelector('[data-id="thumbnail"]');
-        if (thumbnailElement) thumbnailElement.src = post.imageUrl;
+    const thumbnailElement = liElement.querySelector('[data-id="thumbnail"]');
+    if (thumbnailElement) thumbnailElement.src = post.imageUrl;
 
-        return liElement;
-    } catch (error) {
-        console.log('failed create post element', error);
-    }
+    return liElement;
 }
 
 function renderPostList(postList) {
@@ -48,7 +49,7 @@ function renderPostList(postList) {
     try {
         const queryParams = {
             _page: 1,
-            _limit: 6,
+            _limit: 20,
         };
         const { data, pagination } = await postApi.getAll(queryParams);
         renderPostList(data);
